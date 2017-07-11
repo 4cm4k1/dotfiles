145410244823cbceec2833edbf8765eeb5b604ad
Anthony Maki <4cm4k1@gmail.com>
2017-07-11 14:07:16 -0500

    Update system configs

---
+++ dotfiles/piplist.py
@@ -0,0 +1,6 @@
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+     for i in installed_packages])
+for pack in installed_packages_list:
+    print(pack)
